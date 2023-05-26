66341f569456e5222ee98a400e3f25398eac4d94
Behnam Rajabifard <b.rajabifard@gmail.com>
2023-05-26 18:41:45 +0330

    Add place fix

--- resolvers/addManualPlace.js
+++ resolvers/addManualPlace.js
@@ -1,6 +1,7 @@
 import { ManualPlace } from '../models'
 import getTzAndLoc from '../helpers/google/getTzAndLoc'
 import followingList from './followingList'
+import axios from 'axios'
 
 export default async (obj, args, ctx, info) => {
   const {
@@ -12,7 +13,15 @@ export default async (obj, args, ctx, info) => {
   } = args
 
   // Find timezone and exact location based on placeId
-  let { city, fullLocation, timezone } = await getTzAndLoc(placeId)
+  // let { city, fullLocation, timezone } = await getTzAndLoc(placeId)
+  let placeData = await getPlaceData(placeId)
+  if (!placeData) {
+    Raven.captureException(err)
+    console.log(err)
+    return 'error adding'
+  }
+
+  let { city, fullLocation, timezone } = placeData
 
   // Used for UTC
   if (!placeId && !city && inputTimezone) {
@@ -41,4 +50,19 @@ export default async (obj, args, ctx, info) => {
   }
 
   return savedPlace.get({ plain: true })
+}
+
+const getPlaceData = async (place_id) => {
+  try {
+    const url = `https://therepm-server-v2.fly.dev/getPlaceData?place_id=${place_id}`
+    const res = await axios.get(url)
+    const zoneData = res.data
+    if (zoneData.ok === true) {
+      return zoneData
+    } else {
+      return undefined
+    }
+  } catch (error) {
+    return undefined
+  }
 }
